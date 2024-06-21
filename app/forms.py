6bb6b3f83b942e168b08cdb43d68fd3997e2c9d5
whb04 <zjwhb@mail.ustc.edu.cn>
2024-06-21 22:03:57 +0800

    0.2

--- app/forms.py
+++ app/forms.py
@@ -1,23 +1,36 @@
+# app/forms.py
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, DateField, SubmitField
+from wtforms import StringField, IntegerField, FloatField, BooleanField, DateField, SubmitField
 from wtforms.validators import DataRequired
+
+class TeacherForm(FlaskForm):
+    id = StringField('ID', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    gender = IntegerField('Gender', validators=[DataRequired()])
+    title = IntegerField('Title', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 class PaperForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    authors = StringField('Authors', validators=[DataRequired()])
-    journal = StringField('Journal', validators=[DataRequired()])
-    year = IntegerField('Year', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-
-class ProjectForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    lead = StringField('Lead', validators=[DataRequired()])
-    start_date = DateField('Start Date', validators=[DataRequired()])
-    end_date = DateField('End Date', validators=[DataRequired()])
+    source = StringField('Source', validators=[DataRequired()])
+    year = DateField('Year', validators=[DataRequired()])
+    type = IntegerField('Type', validators=[DataRequired()])
+    level = IntegerField('Level', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 class CourseForm(FlaskForm):
+    id = StringField('ID', validators=[DataRequired()])
     title = StringField('Title', validators=[DataRequired()])
-    teacher = StringField('Teacher', validators=[DataRequired()])
-    semester = StringField('Semester', validators=[DataRequired()])
+    hours = IntegerField('Hours', validators=[DataRequired()])
+    nature = IntegerField('Nature', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class ProjectForm(FlaskForm):
+    id = StringField('ID', validators=[DataRequired()])
+    title = StringField('Title', validators=[DataRequired()])
+    source = StringField('Source', validators=[DataRequired()])
+    type = IntegerField('Type', validators=[DataRequired()])
+    total_funding = FloatField('Total Funding', validators=[DataRequired()])
+    start_year = IntegerField('Start Year', validators=[DataRequired()])
+    end_year = IntegerField('End Year', validators=[DataRequired()])
     submit = SubmitField('Submit')
--- app/models.py
+++ app/models.py
@@ -1,21 +1,75 @@
+# app/models.py
 from app import db
+
+class Teacher(db.Model):
+    id = db.Column(db.String(5), primary_key=True)
+    name = db.Column(db.String(256), nullable=False)
+    gender = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.Integer, nullable=False)
+
+    papers = db.relationship('TeacherPaper', back_populates='teacher')
+    courses = db.relationship('TeacherCourse', back_populates='teacher')
+    projects = db.relationship('TeacherProject', back_populates='teacher')
+
 
 class Paper(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(256), nullable=False)
-    authors = db.Column(db.String(256), nullable=False)
-    journal = db.Column(db.String(256), nullable=False)
-    year = db.Column(db.Integer, nullable=False)
+    source = db.Column(db.String(256), nullable=False)
+    year = db.Column(db.Date, nullable=False)
+    type = db.Column(db.Integer, nullable=False)
+    level = db.Column(db.Integer, nullable=False)
 
-class Project(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256), nullable=False)
-    lead = db.Column(db.String(256), nullable=False)
-    start_date = db.Column(db.Date, nullable=False)
-    end_date = db.Column(db.Date, nullable=False)
+    authors = db.relationship('TeacherPaper', back_populates='paper')
+
 
 class Course(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(256), primary_key=True)
     title = db.Column(db.String(256), nullable=False)
-    teacher = db.Column(db.String(256), nullable=False)
-    semester = db.Column(db.String(256), nullable=False)
+    hours = db.Column(db.Integer, nullable=False)
+    nature = db.Column(db.Integer, nullable=False)
+
+    teachers = db.relationship('TeacherCourse', back_populates='course')
+
+
+class Project(db.Model):
+    id = db.Column(db.String(256), primary_key=True)
+    title = db.Column(db.String(256), nullable=False)
+    source = db.Column(db.String(256), nullable=False)
+    type = db.Column(db.Integer, nullable=False)
+    total_funding = db.Column(db.Float, nullable=False)
+    start_year = db.Column(db.Integer, nullable=False)
+    end_year = db.Column(db.Integer, nullable=False)
+
+    teachers = db.relationship('TeacherProject', back_populates='project')
+
+
+class TeacherPaper(db.Model):
+    teacher_id = db.Column(db.String(5), db.ForeignKey('teacher.id'), primary_key=True)
+    paper_id = db.Column(db.Integer, db.ForeignKey('paper.id'), primary_key=True)
+    rank = db.Column(db.Integer, nullable=False)
+    is_corresponding_author = db.Column(db.Boolean, nullable=False)
+
+    teacher = db.relationship('Teacher', back_populates='papers')
+    paper = db.relationship('Paper', back_populates='authors')
+
+
+class TeacherCourse(db.Model):
+    teacher_id = db.Column(db.String(5), db.ForeignKey('teacher.id'), primary_key=True)
+    course_id = db.Column(db.String(256), db.ForeignKey('course.id'), primary_key=True)
+    year = db.Column(db.Integer, nullable=False)
+    semester = db.Column(db.Integer, nullable=False)
+    hours_taken = db.Column(db.Integer, nullable=False)
+
+    teacher = db.relationship('Teacher', back_populates='courses')
+    course = db.relationship('Course', back_populates='teachers')
+
+
+class TeacherProject(db.Model):
+    teacher_id = db.Column(db.String(5), db.ForeignKey('teacher.id'), primary_key=True)
+    project_id = db.Column(db.String(256), db.ForeignKey('project.id'), primary_key=True)
+    rank = db.Column(db.Integer, nullable=False)
+    funding_taken = db.Column(db.Float, nullable=False)
+
+    teacher = db.relationship('Teacher', back_populates='projects')
+    project = db.relationship('Project', back_populates='teachers')
