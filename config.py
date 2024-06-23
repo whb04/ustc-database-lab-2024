eb5daddcc395fe7b12b988f4dd857e31093c5a60
whb04 <zjwhb@mail.ustc.edu.cn>
2024-06-23 11:43:33 +0800

    1.2

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1@localhost/lab3'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password@localhost/database'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
 
